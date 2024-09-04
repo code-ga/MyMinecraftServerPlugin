9cbbe77a23d87c56c21be447fbc9c6fb642b3a70
code ngu nhất quả đất <72681678+code-ga@users.noreply.github.com>
2024-09-04 09:43:43 +0700

    Create Home_TP.java

---
+++ src/main/java/com/codega/nbgplugin/Home_TP.java
@@ -0,0 +1,29 @@
+package org.thedt.the_dt_plugin;
+
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class HOME_TP implements CommandExecutor {
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] split) {
+        if (!(sender instanceof Player player)) {
+            return false;
+        }
+        if (!player.getWorld().getName().startsWith("world")) {
+            player.sendMessage("This command only works in the world");
+            return true;
+        }
+        Location bed = player.getRespawnLocation();
+        if (bed == null) {
+            player.teleport(player.getWorld().getSpawnLocation());
+            player.sendMessage("Teleported to spawn");
+            return true;
+        }
+        player.teleport(bed);
+        player.sendMessage("Teleported to spawn");
+        return true;
+    }
+
+}
